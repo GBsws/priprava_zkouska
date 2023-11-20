8869cd862ce414bd0126b43869ab8f1488930dfb
Gabriela Biswasová <pracho.dayat@seznam.cz>
2023-11-20 13:44:32 +0100

    objekty

---
+++ src/data.jsx
@@ -0,0 +1,32 @@
+export const sperky = [
+  {
+    nazev: "kočka",
+    cislo: "17",
+    pismeno: "",
+    cislo_fotky: "17",
+    typ:["brož","spona na oděv"],
+    material:[ "chirurgická ocel","drahé kameny"],
+    technika:[ "kované","tepané"],
+    opracovani:["leštěné"],
+    lokalita: ["české"],
+    kamen:[ "železitý křemen","olivín"],
+    motiv: ["zoomorfní"],
+    kolekce: ["Země česká"],
+  },
+  {
+    nazev: "blankytně",
+    cislo: "18",
+    pismeno: "",
+    cislo_fotky: "18",
+    typ: ["náramek"],
+    material: ["chirurgická ocel","drahé kameny"],
+    technika: ["kované","tepané"],
+    opracovani: ["výbrusy"],
+    lokalita: ["zahraniční"],
+    kamen: ["křišťál","modrý křemen"],
+    motiv: ["abstraktní a jiné"],
+    kolekce: ["Země česká"],
+  }
+];
+																	
+ 																						
